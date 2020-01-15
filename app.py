8a86ceb96996dcae41d6243e9dc1cbc7b281bc97
freeze <n0936170164@gmail.com>
2020-01-15 21:25:11 +0800

    ok

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message) 
     else:
         message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, len(message))
+        line_bot_api.reply_message(event.reply_token, str(len(message)))
 
 
 import os
